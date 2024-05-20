d7d6e641007d3bff3ce21dfcc1ba6e4a1ee29032
EnzoKurt <142853969+EnzoKurt@users.noreply.github.com>
2024-05-20 19:55:12 -0300

    Update 7.cs

--- L4/7.cs
+++ L4/7.cs
@@ -1,9 +1,35 @@
-﻿using Sistem;
+using System;
 
-public class Program
-{
-    public static void Main()
-    {
-        Console.WriteLine("Hello, World!");
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Digite a quantidade de alunos:");
+    int a=int.Parse(Console.ReadLine());//coleta do usuário
+    notas(a);//chama a função
+  }
+  static void notas(int x){
+    for(int i=0;i<x;i++){
+      Console.WriteLine ("Digite a nota do aluno:");
+      int a=int.Parse(Console.ReadLine());//coleta a nota do aluno
+      if(a<=39){//se for menor ou igual a 39, entâo imprime na tela:
+        Console.WriteLine("Conceito: F");
+      }
+      else if(a<=59){//se for maior que 39 e menor ou igual a 59, entâo imprime na tela
+        Console.WriteLine("Conceito: E");
+      }
+      else if(a<=69){//se for maior que 59 e menor ou igual a 69, entâo imprime na tela
+        Console.WriteLine("Conceito: D");
+      }
+      else if(a<=79){//se for maior que 69 menor ou igual a 79, entâo imprime na tela
+        Console.WriteLine("Conceito: C");
+      }
+      else if(a<=89){//se for maior que 79 e menor ou igual a 89, entâo imprime na tela
+        Console.WriteLine("Conceito: B");
+      }
+      else{//se for maior que 89, entâo imprime na tela
+        Console.WriteLine("Conceito: A");
+      }
+      Console.ReadKey();
+      Console.Clear();
     }
+  }
 }
