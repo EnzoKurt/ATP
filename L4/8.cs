dd668f98ee8d45bf24d6c8106fb69ccb844a62c5
EnzoKurt <142853969+EnzoKurt@users.noreply.github.com>
2024-05-22 11:32:56 -0300

    Update 8.cs

--- L4/8.cs
+++ L4/8.cs
@@ -1,9 +1,17 @@
-﻿using Sistem;
+using System;
 
-public class Program
-{
-    public static void Main()
-    {
-        Console.WriteLine("Hello, World!");
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Digite um valor inteiro positivo:");
+    int n=int.Parse(Console.ReadLine());//coleta número do usuário
+    Console.WriteLine("valor de s é:{0}",Formula(n));//imprime resultado com o valor retornado pela função acionada
+  }
+  static double Formula(int x){
+    double s=0;
+    for(int i=1;i<x;i++){
+     s+=((i*i)+1)/i+3;//aplica-se a formula para calcular s 
+     Console.WriteLine(s);
     }
+    return s;//retorna valor encontrado em s
+  }
 }
