bb6d57df9a0e78ccbebfdf2952bb81a2e01255fc
EnzoKurt <142853969+EnzoKurt@users.noreply.github.com>
2024-06-04 00:35:20 +0000

    Lista 5

---
+++ LISTA 05/Exerrcicio02.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Exercicio02 {
+    public void Rodar() {
+        Console.WriteLine("Exercício 02 da Lista 05 de ATP");
+    }
+}
---
+++ LISTA 05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program {
+    public static void Main() {
+        Console.WriteLine("Lista 05 de ATP");
+        Console.WriteLine("Informe:");
+        Console.WriteLine("1) Para executar o exercício 1 da lista 05 de ATP.");
+        Console.WriteLine("2) Para executar o exercício 2 da lista 05 de ATP.");
+        int escolha = int.Parse(Console.ReadLine());
+
+        switch(escolha){
+          case 1:
+            Exercicio01 exercicio01 = new Exercicio01();
+            exercicio01.Rodar();
+            break;
+          case 2:
+            Exercicio02 exercicio02 = new Exercicio02();
+            exercicio02.Rodar();
+            break;
+          default:
+            Console.WriteLine("Opção inválida.");
+            break;
+        }
+    }
+}
