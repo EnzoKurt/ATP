f202c12fc08ae9d4e21843d7c174fcb223a8e194
EnzoKurt <142853969+EnzoKurt@users.noreply.github.com>
2024-06-05 17:41:57 -0300

    Update Exercicio04.cs

--- LISTA 05/Exercicio04.cs
+++ LISTA 05/Exercicio04.cs
@@ -1,7 +1,43 @@
 using System;
 
-public class Exercicio04 {
-    public void Rodar() {
-        Console.WriteLine("Exercício 01 da Lista 05 de ATP");
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] v1 = new int[10];//declara os dois vetores
+        int[] v2 = new int[10];
+        PreencheVetores(v1, v2);//aciona o procedimento
+        Console.WriteLine();//salta uma linha
+        JuntaVetor(v1, v2);//aciona o procedimento
+    }
+    static void PreencheVetores(int[] a, int[] b)
+    {
+        Random r = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            a[i] = r.Next(1, 101); //sorteia os valores de cada posição do vetor
+            Console.WriteLine("Va= "+a[i]);//imprime cada valor 
+            b[i] = r.Next(1, 101); //sorteia os valores de cada posição do vetor
+            Console.WriteLine("Vb= "+b[i]); //imprime cada valor
+        }
+    }
+    static void JuntaVetor(int[] x, int[] y)
+    {
+        int[] vetorao = new int[20];//cria o vetor que irá juntar os outros dois do procedimento
+        for (int j = 0; j < vetorao.Length; j++)
+        {
+            if (j % 2 != 0)//se a posição for ímpar, então o vetor tomará como valor dessa posição o primeiro valor do vetor 1, e assim por diante
+            {
+                vetorao[j] = x[j/2];//posição divide por 2 pois o tamanho do vetor referido é a metade do vetorao
+            }
+            else //se a posição for par, então o vetor tomará como valor dessa posição o primeiro valor do vetor 2, e assim por diante
+            {
+                vetorao[j] = y[j/2];
+            }
+        }
+        foreach (int item in vetorao)
+        {
+            Console.Write(item + " ");//imprime cada valor do vetor
+        }
     }
 }
