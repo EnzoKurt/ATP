068ab2ea7f07660e6ae5695c0ea5546050101a6b
EnzoKurt <142853969+EnzoKurt@users.noreply.github.com>
2024-06-05 14:28:19 -0300

    Update Exercicio03.cs

--- LISTA 05/Exercicio03.cs
+++ LISTA 05/Exercicio03.cs
@@ -1,7 +1,36 @@
 using System;
 
-public class Exercicio03 {
-    public void Rodar() {
-        Console.WriteLine("Exercício 01 da Lista 05 de ATP");
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Notas alunos 2024");
+    int a=10;//tamanho do vetor
+    preencher(a);
+  }
+  static void preencher (int a){
+    int[] v= new int[a];//criação do vetor
+      for(int i=0;i<v.Length;i++){
+        Console.WriteLine("Digite um número (positivo ou negativo):");//coleta o número do usuário
+        v[i]=int.Parse(Console.ReadLine());//cada posição do vetor toma a nota digitada pelo usuário
+      }
+    Console.WriteLine("Os números negativos são:");
+    foreach(int item in vetor(v)){
+      Console.Write(item + " ");
     }
+      //aciona a função
+  }
+  static int[] vetor(int[] x){
+    int a=0, contador=0;
+    foreach(int item in x){//"item" percorre o vetor
+      if(item<0){
+        a++;//a é o contador de números negativos no vetor x
+      }
+    }
+    int[] neg=new int[a];//vetor de negativos será do tamanho do valor de a
+    for(int i=0;i<neg.Length;i++){
+      if(x[i]<0){
+        neg[contador++]=x[i];//cada vez que o programa acha um número negativo dentro do vetor x, ele introduz em uma posição do vetor de negativos
+      }
+    }
+    return neg;//retorna o vetor de negativos
+  }
 }
