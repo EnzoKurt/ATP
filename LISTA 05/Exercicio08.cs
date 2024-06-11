e816c96b8db13d5fdf508c1e0a071bcc94cc537b
EnzoKurt <142853969+EnzoKurt@users.noreply.github.com>
2024-06-10 22:11:33 -0300

    Update Exercicio08.cs

--- LISTA 05/Exercicio08.cs
+++ LISTA 05/Exercicio08.cs
@@ -1,7 +1,36 @@
 using System;
 
 public class Exercicio08 {
-    public void Rodar() {
-        Console.WriteLine("Exercício 01 da Lista 05 de ATP");
+    int[,] matriz = new int[4, 4];
+  
+  public void Rodar () {
+    Preenche();
+    Imprime();
+    SomaAbaixoDiagonalPrincipal();
+  }
+  
+  public void Preenche() {
+    Random sort = new Random();
+    
+    for(int i = 0; i < 4; i++) 
+      for(int j = 0; j < 4; j++) 
+        matriz[i, j] = sort.Next(0,10);
+  }
+  
+  public void Imprime() {
+    for(int i = 0; i < 4; i++) {
+      for(int j = 0; j < 4; j++) 
+        Console.Write (matriz[i, j] + "   ");
+      Console.WriteLine();
     }
+  }
+
+  public void SomaAbaixoDiagonalPrincipal(){
+    int soma = 0;
+    
+    for(int i = 0; i < 4; i++) 
+      for(int j = 0; j < i; j++) 
+        soma += matriz[i, j];
+    Console.WriteLine("Soma dos elementos abaixo da diagonal principal: " + soma);
+  }
 }
